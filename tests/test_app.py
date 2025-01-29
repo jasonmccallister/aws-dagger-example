0c3116f576baed0fdb820c36f245ea5b0b1a16c7
Jason McCallister <jason@dagger.io>
2025-01-29 15:49:14 -0500

    init
    Signed-off-by: Jason McCallister <jason@dagger.io>

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get("/api")
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Hello, Flask!"}
